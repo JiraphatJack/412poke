f66c9a66a684f55fdec00eb4d64129af0399918a
davidwang7777 <76135199+davidwang7777@users.noreply.github.com>
2022-11-19 20:43:04 -0700

    updated gui file

--- gui.py
+++ gui.py
@@ -7,14 +7,13 @@ display.geometry('500x250')
 
 background = tkinter.PhotoImage(file="pokemonLogo.gif") #give it a better color later
 bgimg = Label(display, i=background)
-bgimg.img_ref = background
 bgimg.pack()
 
-titleLabel = Label(display, text = 'Welcome to the Pokedex API! Click a button.', width = 100, height = 75)
-titleLabel.pack()
+titleLabel = Label(display, text = 'Welcome to the Pokedex API! Click a button.', width = 35, height = 1)
+titleLabel.place(x=125, y= 50)
 
 def func(): tkinter.messagebox.showinfo("Button was clicked") #display graph here
-enterButton = Button(display, text= "Enter", width = 12, height = 5, command=func)
-enterButton.place(x=180, y= 125)
+enterButton = Button(display, text= "Enter", width = 5, height = 2, command=func)
+enterButton.place(x=228, y= 100)
 
 display.mainloop()
