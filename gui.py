eec55124d6668aabe17ed0e6ed0f017d5198e68a
bltsai13 <99511049+bltsai13@users.noreply.github.com>
2022-11-20 16:56:51 -0700

    Update gui.py
    Get rid of second window and introduce closing for connection to database

--- gui.py
+++ gui.py
@@ -50,6 +50,10 @@ def postgresql_to_dataframe(conn, select_querry, column_names):
 conn = connect(param_dic)
 # Execute the "SELECT *" query
 
+def whenClosed():
+    conn.close()
+    display.destroy()
+
 display = tkinter.Tk()
 display.geometry('500x250')
 
@@ -59,15 +63,15 @@ display.geometry('500x250')
 
 titleLabel = Label(display, text = 'Welcome to the Pokedex API! Click a button.', width = 35, height = 1)
 titleLabel.place(x=250, y= 20)
+col = ["name","capture_rate", "height", "weight"]
+pokemon = postgresql_to_dataframe(conn, "select name,capture_rate,height,weight from pokemon", col)
 
 def openHeightWeightScreen():
-    screen = Toplevel(display)
-    screen.title("Height-Weight Scatter Plot")
-    screen.geometry('750x375')
-    Label(screen, text = "Height-Weight Scatter Plot").pack()
+    #screen = Toplevel(display)
+    #screen.title("Height-Weight Scatter Plot")
+    #screen.geometry('750x375')
+    #Label(screen, text = "Height-Weight Scatter Plot").pack()
     #insert graph or image of graph here
-    col = ["name","capture_rate", "height", "weight"]
-    pokemon = postgresql_to_dataframe(conn, "select name,capture_rate,height,weight from pokemon", col)
     print(pokemon)
     plt.scatter(pokemon['height'],pokemon['weight'])
     plt.xlabel("height")
@@ -76,24 +80,53 @@ def openHeightWeightScreen():
     plt.show()
 
 def openMoveTypeScreen():
-    screen = Toplevel(display)
-    screen.title("Move Type Bar Graph")
-    screen.geometry('750x375')
-    Label(screen, text = "Move Type Bar Graph").pack()
+    #screen = Toplevel(display)
+    #screen.title("Move Type Bar Graph")
+    #screen.geometry('750x375')
+    #Label(screen, text = "Move Type Bar Graph").pack()
     #insert graph or image of graph here
+    moves = postgresql_to_dataframe(conn, '''SELECT types.type_name, x.num
+                                            FROM (SELECT types.type_id as id,count(*) as num 
+                                                FROM moves, types
+                                                WHERE types.type_id = moves.type_id
+                                                GROUP BY types.type_id) as x, types
+                                            WHERE types.type_id = x.id
+                                            ORDER BY types.type_id''', ["type_name","count"])
+    print(moves)
+    fig = plt.figure(figsize = (20, 5))
+    plt.bar(moves['type_name'],moves['count'], width = 0.6)
+    plt.xlabel("type")
+    plt.ylabel("number of move")
+    plt.title("number of move in each type")
+    plt.show()
 
 def openPokemonTypeScreen():
-    screen = Toplevel(display)
-    screen.title("Pokemon Type Bar Graph")
-    screen.geometry('750x375')
-    Label(screen, text = "Pokemon Type Bar Graph").pack()
+    #screen = Toplevel(display)
+    #screen.title("Pokemon Type Bar Graph")
+    #screen.geometry('750x375')
+    #Label(screen, text = "Pokemon Type Bar Graph").pack()
     #insert graph or image of graph here
+    types = postgresql_to_dataframe(conn, '''SELECT types.type_name, x.num
+                                            FROM (SELECT types.type_id as id,count(*) as num 
+                                                FROM types, hastype
+                                                WHERE types.type_id = hastype.type_id
+                                                GROUP BY types.type_id) as x, types
+                                            WHERE types.type_id = x.id
+                                            ORDER BY types.type_id''', ["type_name","count"])
+
+    print(types)
+    fig = plt.figure(figsize = (20, 5))
+    plt.bar(types['type_name'],types['count'], width = 0.6)
+    plt.xlabel("type")
+    plt.ylabel("number of pokemon")
+    plt.title("number of pokemon in each type")
+    plt.show()
 
 def CaptureRateWeightScreen():
-    screen = Toplevel(display)
-    screen.title("Capture Rate vs Weight Scatter Plot")
-    screen.geometry('750x375')
-    Label(screen, text = "Capture Rate vs Weight Scatter Plot").pack()
+    #screen = Toplevel(display)
+    #screen.title("Capture Rate vs Weight Scatter Plot")
+    #screen.geometry('750x375')
+    #Label(screen, text = "Capture Rate vs Weight Scatter Plot").pack()
     #insert graph or image of graph here
     #capture rate and weight scatter plot
     plt.scatter(pokemon['capture_rate'],pokemon['weight'])
@@ -103,18 +136,40 @@ def CaptureRateWeightScreen():
     plt.show()
 
 def NumMovesScreen():
-    screen = Toplevel(display)
-    screen.title("Number of Learnable Moves")
-    screen.geometry('750x375')
-    Label(screen, text = "Number of Learnable Moves").pack()
+    #screen = Toplevel(display)
+    #screen.title("Number of Learnable Moves")
+    #screen.geometry('750x375')
+    #Label(screen, text = "Number of Learnable Moves").pack()
     #insert graph or image of graph here
+    move_per_pokemon = postgresql_to_dataframe(conn, '''SELECT pokemon.pokedex_number, x.num
+                                                    FROM (SELECT pokemon.pokedex_number as id,count(*) as num 
+                                                                FROM pokemon, (SELECT DISTINCT movepool.pokedex_number,
+                                                                            movepool.move_id 
+                                                                            FROM movepool) as m1
+                                                                WHERE pokemon.pokedex_number = m1.pokedex_number
+                                                                GROUP BY pokemon.pokedex_number) as x, pokemon
+                                                            WHERE x.id = pokemon.pokedex_number
+                                                            ORDER BY x.id''', ["pokedex_number","count"])
+
+    #histogram for pokemon move
+    fig, axs = plt.subplots(1, 1, figsize = (10,7), tight_layout = True)
+    axs.hist(move_per_pokemon['count'],bins = 30)
+    plt.xlabel("number of move")
+    plt.ylabel("number of pokemon")
+    plt.title("number of moves that pokemon can learn")
+    plt.show()
 
 def CaptureRateHeightScreen():
-    screen = Toplevel(display)
-    screen.title("Capture Rate vs Height Scatter Plot")
-    screen.geometry('750x375')
-    Label(screen, text = "Capture Rate vs Height Scatter Plot").pack()
+    #screen = Toplevel(display)
+    #screen.title("Capture Rate vs Height Scatter Plot")
+    #screen.geometry('750x375')
+    #Label(screen, text = "Capture Rate vs Height Scatter Plot").pack()
     #insert graph or image of graph here
+    plt.scatter(pokemon['capture_rate'],pokemon['height'])
+    plt.xlabel("capture rate")
+    plt.ylabel("height")
+    plt.title("Scatter plot of capture rate VS height")
+    plt.show()
 
 heightWeightButton = Button(display, text="Height-Weight Comparison", command = openHeightWeightScreen)
 heightWeightButton.place(x=20, y=50)
@@ -134,4 +189,5 @@ captureRateHeightButton.place(x=225, y=100)
 NumMovesButton = Button(display, text="Number of Learnable Moves", command = NumMovesScreen)
 NumMovesButton.place(x=225, y=150)
 
+display.protocol("WM_DELETE_WINDOW", whenClosed)
 display.mainloop()
